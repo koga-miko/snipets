6d36e9c70732abca50d92cd63e05ba2384830330
yoshihama0141@gmail.com <yoshihama0141@gmail.com>
2020-12-01 23:12:25 +0000

    mod renice shell

--- thread_test/renice_all_threads.sh
+++ thread_test/renice_all_threads.sh
@@ -1,11 +1,11 @@
 searchname="main$"
-reniceval=-13
+reniceval=-20
 
 ps -A -L -o pid,lwp,pri,nice,cmd | grep -E "${searchname}"
 
 ps -A -L -o lwp,nice,cmd | grep -E "${searchname}" | while read line
 do
-    cur_niceval=`echo $line | sed -n "s/^ *[-0-9][-0-9]*  *\([-0-9][-0-9]*\)  *.*$/\1/p"`
+    cur_niceval=`echo $line | sed -n "s/^ *[0-9][0-9]*  *\([-0-9][0-9]*\)  *.*$/\1/p"`
     thread_id=`echo $line | sed -n "s/^ *\([0-9][0-9]*\)  *.*$/\1/p"`
     echo renice -n $cur_niceval + $reniceval $thread_id
     sudo renice -n $(($cur_niceval + $reniceval)) $thread_id
